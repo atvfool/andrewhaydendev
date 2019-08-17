63ddd1b6a7d6e6409cf3b1cb6f72db737c116379
Andrew <atvfool@gmail.com>
2019-08-17 02:57:24 -0400

    Added FontAwesome glyph pack and changed formatting of the pages

--- andrewhaydendev/Controllers/ProjectsController.cs
+++ andrewhaydendev/Controllers/ProjectsController.cs
@@ -21,76 +21,9 @@ namespace andrewhaydendev.Controllers
         // GET: Projects/Details/5
         public ActionResult Details(int id)
         {
-            return View();
-        }
+            DatabaseContext context = HttpContext.RequestServices.GetService(typeof(DatabaseContext)) as DatabaseContext;
 
-        // GET: Projects/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Projects/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Projects/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Projects/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Projects/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Projects/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            return View(context.GetProjectByID(id));
         }
     }
 }
--- andrewhaydendev/Models/DatabaseContext.cs
+++ andrewhaydendev/Models/DatabaseContext.cs
@@ -19,7 +19,7 @@ namespace andrewhaydendev.Models
         {
             return new MySqlConnection(ConnectionString);
         }
-
+        
         public List<ProjectModel> GetAllProjects()
         {
             List<ProjectModel> Projects = new List<ProjectModel>();
@@ -27,7 +27,7 @@ namespace andrewhaydendev.Models
             using (MySqlConnection conn = GetConnection())
             {
                 conn.Open();
-                MySqlCommand cmd = new MySqlCommand("SELECT * FROM Projects", conn);
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM projects", conn);
 
                 using(var reader = cmd.ExecuteReader())
                 {
@@ -36,13 +36,47 @@ namespace andrewhaydendev.Models
                         Projects.Add(new ProjectModel()
                         {
                             ID = Convert.ToInt32(reader["ID"]),
-                            Name = reader["Name"].ToString()
-                        }) ;
+                            Name = reader["Name"].ToString(),
+                            RepositoryLink = reader["RepositoryLink"].ToString(),
+                            CompiledProjectLink = reader["CompiledProjectLink"].ToString(),
+                            ActiveDevelopment = Convert.ToBoolean(reader["ActiveDevelopment"]),
+                            Description = reader["Description"].ToString(),
+                            ImageURL = reader["ImageURL"].ToString().Equals(string.Empty) ? "/static/no-image-available.png" : reader["ImageURL"].ToString(),
+                    }) ;
                     }
                 }
             }
 
             return Projects;
+        }
+
+        public ProjectModel GetProjectByID(int ID)
+        {
+            ProjectModel pm = new ProjectModel();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM projects WHERE ID = @ProjectID", conn);
+                cmd.Parameters.Add(new MySqlParameter() { ParameterName = "ProjectID", Value = ID });
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        pm.ID = Convert.ToInt32(reader["ID"]);
+                        pm.Name = reader["Name"].ToString();
+                        pm.RepositoryLink = reader["RepositoryLink"].ToString();
+                        pm.CompiledProjectLink = reader["CompiledProjectLink"].ToString();
+                        pm.ActiveDevelopment = Convert.ToBoolean(reader["ActiveDevelopment"]);
+                        pm.Description = reader["Description"].ToString();
+                        pm.ImageURL = reader["ImageURL"].ToString().Equals(string.Empty) ? "/static/no-image-available.png" : reader["ImageURL"].ToString();
+                        break; // This isn't necessary but I'm paranoid
+                    }
+                }
+            }
+
+                return pm;
         }
     }
 }
